c68bef3d7b714f624f5624f198d7f7f63f01f5b2
vikaskumar098 <vikash76518@gmail.com>
2026-09-17 06:54:25 +0530

    Add Java learning programs

---
+++ JavaBasics.java
@@ -0,0 +1,183 @@
+import java.util.Scanner;
+
+public class JavaBasics {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //output in java 
+        // System.out.println("Hello, World!");
+
+        // print a pattern
+        // System.out.println("****");
+        // System.out.println("***");
+        // System.out.println("**");
+        // System.out.println("*");
+
+        //data types in java 
+        // byte b = 1 ;
+        // System.out.println(b);
+        // int a = 10;
+        // System.out.println(a);
+        // float price = 1.1f;
+        // System.out.println(price);
+        // char ch = 'a';
+        // System.out.println(ch);
+        // boolean var = false;
+        // System.out.println(var);
+
+        //sum of a & b 
+        // int a = 10;
+        // int b = 5;
+        // int sum = a+b;
+        // System.out.println(sum);
+
+        //input in java 
+        // String input = sc.next();
+        // System.out.println(input);
+
+        // String name = sc.nextLine();
+        // System.out.println(name);
+
+        // int number = sc.nextInt();
+        // System.out.println(number);
+        
+        // float price = sc.nextFloat();
+        // System.out.println(price);
+
+        //sum of 2 numbers (input from user)
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // int sum = a+b;
+        // System.out.println(sum);
+
+
+        //product a&b
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // int product = a * b;
+        // System.out.println(product);
+
+        //area of a circle
+        // float rad = sc.nextFloat();
+        // float area = 3.14f * rad * rad;
+        // System.out.println(area);
+
+
+        //if else statement
+        // System.out.println("enter your age :");
+        // int age = sc.nextInt();
+        
+
+        // if(age >= 18){
+        //     System.out.println("adult");
+        // }
+
+        // else if (age > 13 && age < 18) {
+        //     System.out.println("teenager");
+        // }
+
+        // else{
+        //     System.out.println("not adult");
+        // }
+
+
+
+        //largest or A AND B
+        // System.out.println("enter your number:");
+        // int A = sc.nextInt();
+        // int B = sc.nextInt();
+
+        // if (A >= B) {
+        //     System.out.println("A is largest");
+
+            
+        // }else{
+        //     System.out.println("B is largest");
+        // }
+        
+
+        //ODD EVEN NUMBER FIND
+        // System.out.println("Enter your number :");
+        // int number = sc.nextInt();
+        // if(number % 2 == 0){
+        //     System.out.println("EVEN");
+        // }else{
+        //     System.out.println("ODD");
+        // }
+
+        //Income tax calculator
+
+        // System.out.println("Enter your income:");
+        // int income = sc.nextInt();
+        // if (income <= 5) {
+        //     System.out.println("0% TAX");
+            
+        // }else if (income > 5 && income < 10) {
+        //     System.out.println("20% TAX");
+        // }else{
+        //     System.out.println("30% TAX");
+        // }
+
+        //ternary operator
+        // int number = 4;
+        // String type = ((number % 2) == 0)? "even": "odd";
+        // System.out.println(type);
+
+
+        //fail or pass check using ternary operator
+        // System.out.println("Enter your marks:");
+        // int marks = sc.nextInt();
+        // String result = ((marks >= 33))? "pass": "fail";
+        // System.out.println(result);
+
+
+        //switch statement 
+        // System.out.println("Enter your number :");
+        // int number = sc.nextInt();
+
+        // switch (number) {
+        //     case 1 : System.out.println("hello");
+                
+        //         break;
+        //     case 2 : System.out.println("hey");
+        //         break;
+
+        //     case 3 : System.out.println("namste");
+        //         break;
+        
+        //     default: System.out.println("bye");
+        //         break;
+        // }
+
+        //calculator prohram 
+        // System.out.println("enter a:");
+        // int a = sc.nextInt();
+        // System.out.println("enter b:");
+        // int b = sc.nextInt();
+        // System.out.println("enter operator");
+        // char operator = sc.next().charAt(0);
+
+        // switch (operator) {
+        //     case '+': System.out.println("answer is:" + a+b);
+                
+        //         break;
+
+        //     case '-': System.out.println("answer is:" + (a-b));
+                
+        //         break;
+        //     case '*': System.out.println("answer is:" + a*b);
+                
+        //         break;
+        //     case '/': System.out.println("answer is:" + a/b);
+                
+        //         break;
+        //     case '%': System.out.println("answer is:" + a%b);
+                
+        //         break;    
+        //     default: System.out.println("wrong entry");;
+        // }
+
+        
+
+    }
+}
