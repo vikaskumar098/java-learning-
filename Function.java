8fa3bc6952c6e181390cd51847511d24cf3e0e3d
vikaskumar098 <vikash76518@gmail.com>
2026-09-24 03:29:54 +0530

    Function arguments passing

--- Function.java
+++ Function.java
@@ -4,16 +4,17 @@ import java.util.*;
 
 public class Function {
 
-    public static void Calculatesum(){
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int sum = a + b ;
-        System.out.println(sum);
+    public static void Calculatesum(int num1, int num2){
+        int sum = num1  + num2 ;
+        System.out.println("sum is : " + sum);
     }
 
     public static void main(String[] args) {
-        Calculatesum();
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+        Calculatesum(a, b);
     }
 }
 
