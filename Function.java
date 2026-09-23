b8e2510db048ab43c8d5751eb3dd6c30a29ede11
vikaskumar098 <vikash76518@gmail.com>
2026-09-24 03:23:21 +0530

    Function arguments

--- Function.java
+++ Function.java
@@ -1,11 +1,34 @@
+import java.util.*;
+
+
+
 public class Function {
-    public static void printHello() {
-        System.out.println("Hello!!");
-        return;
+
+    public static void Calculatesum(){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a + b ;
+        System.out.println(sum);
     }
 
     public static void main(String[] args) {
-        printHello(); 
+        Calculatesum();
     }
-    
 }
+
+
+// public class Function {
+//     public static void printHello() {
+//         System.out.println("Hello!!");
+//         return;
+//     }
+
+//     public static void main(String[] args) {
+//         printHello(); 
+//     }
+    
+// }
+
+
+
