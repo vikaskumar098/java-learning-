625bfbf6903d2d65a64fba26af7110b667d03d7a
vikaskumar098 <vikash76518@gmail.com>
2026-09-26 07:41:55 +0530

    Function arguments add new code like product funcaton

--- Function.java
+++ Function.java
@@ -4,17 +4,32 @@ import java.util.*;
 
 public class Function {
 
-    public static void Calculatesum(int num1, int num2){
+    public static int Calculatesum(int num1, int num2){ //formal parametrs
         int sum = num1  + num2 ;
-        System.out.println("sum is : " + sum);
+        return sum;
+    }
+
+    public static int Multiply(int a, int b){
+        int Multiply = a * b;
+        return Multiply;
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+        int a = 3;
+        int b = 5;
+        int prod = Multiply(a, b);
+        System.out.println("a * b = " + prod);
+        prod = Multiply(10, 20);
+        System.out.println("a * b = " + prod);
+
+
+
+        // Scanner sc = new Scanner(System.in);
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
         
-        Calculatesum(a, b);
+        // int sum = Calculatesum(a, b);
+        // System.out.println("sum is : " + sum);
     }
 }
 
