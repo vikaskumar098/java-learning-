7f772ea9a4c9d73ef82c3b95e2e56c2135eb84bd
vikaskumar098 <vikash76518@gmail.com>
2026-09-26 12:40:31 +0530

    Function arguments add new code like binomial

--- Function.java
+++ Function.java
@@ -18,15 +18,24 @@ public class Function {
         int f = 1;
 
         for(int i = 1; i<=n; i++){
-            f = f * n;
+            f = f * i;
         }
 
         return f;
     }
 
+    public static int binCoeff(int n, int r){
+        int fact_n = factorial(n);
+        int fact_r = factorial(r);
+        int fact_nmr = factorial(n-r);
+
+        int binCoeff = fact_n/(fact_r * fact_nmr);
+        return binCoeff;
+    }
+
     public static void main(String[] args) {
 
-        System.out.println(factorial(4));
+        System.out.println(binCoeff(5, 2));
 
         
         // int a = 3;
